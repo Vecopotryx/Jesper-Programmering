3f66209ec72c7d889d07adf164465afc1bd3e040
Vecopotryx <repsejk@gmail.com>
2019-09-13 09:36:18 +0200

    Mer frågor

--- Hemläxor/v37/src/com/stuff/Main.java
+++ Hemläxor/v37/src/com/stuff/Main.java
@@ -20,6 +20,17 @@ public class Main {
             System.out.println("Ange något rimligt värde...");
         }
 
+        System.out.print("Vad är din address? ");
+        String address = input.nextLine(); // vet inte varför den beter sig så konstigt, men JRE ignorerar den första scannern efter try/catch..
+
+        System.out.print("Vad är ditt postnummer? ");
+        String postalCode = input.nextLine();
+
+        System.out.print("Vilken stad bor du i? ");
+        String city = input.nextLine();
+
+        System.out.print("Vad är ditt telefonnummer? ");
+        String phone = input.nextLine();  // ville inte lagra det i en string, men jaja.
 
         System.out.println(name);
         System.out.println(age);
