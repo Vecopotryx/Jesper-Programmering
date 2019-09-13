5cef921d4196ebb1f38654bed1ebbf65cffcb464
Vecopotryx <repsejk@gmail.com>
2019-09-13 19:28:29 +0200

    Fixa udda beteende

--- Hemläxor/v37/src/com/stuff/Main.java
+++ Hemläxor/v37/src/com/stuff/Main.java
@@ -4,14 +4,31 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static Scanner input = new Scanner(System.in);
+    public static byte age; // definera age.
+    public static int phone;
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
         System.out.print("Vad är ditt namn? ");
         String name = input.nextLine();
 
-        byte age = 0; // definera age
+        getAge();
 
+        System.out.print("Vad är din address? ");
+        String address = input.next();
+
+        System.out.print("Vad är ditt postnummer? ");
+        String postalCode = input.next();
+
+        System.out.print("Vilken stad bor du i? ");
+        String city = input.next();
+
+        getPhone();
+
+    }
+
+    public static Byte getAge() {
         try {
             System.out.print("Hur gammal är du? ");
             age = input.nextByte();
@@ -19,22 +36,17 @@ public class Main {
         catch(Exception e) {
             System.out.println("Ange något rimligt värde...");
         }
+        return age;
+    }
 
-        System.out.print("Vad är din address? ");
-        String address = input.nextLine(); // vet inte varför den beter sig så konstigt, men JRE ignorerar den första scannern efter try/catch..
-
-        System.out.print("Vad är ditt postnummer? ");
-        String postalCode = input.nextLine();
-
-        System.out.print("Vilken stad bor du i? ");
-        String city = input.nextLine();
-
-        System.out.print("Vad är ditt telefonnummer? ");
-        String phone = input.nextLine();  // ville inte lagra det i en string, men jaja.
-
-        System.out.println(name);
-        System.out.println(age);
-
-
+    public static Integer getPhone() {
+        try {
+            System.out.print("Vad är ditt telefonnummer? ");
+            phone = input.nextInt();
+        }
+        catch(Exception e) {
+            System.out.println("Det där liknar inte ett telefonnummer...");
+        }
+        return phone;
     }
 }
