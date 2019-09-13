7f35ba15ab4b58eb6448af70f9af2592574b7d13
Vecopotryx <repsejk@gmail.com>
2019-09-13 08:51:52 +0200

    Create Scanner

--- Hemläxor/v37/src/com/stuff/Main.java
+++ Hemläxor/v37/src/com/stuff/Main.java
@@ -1,8 +1,11 @@
 package com.stuff;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         
     }
 }
