bb00964564fc146527cee52d33025022c358c0da
Vecopotryx <repsejk@gmail.com>
2019-09-13 08:55:49 +0200

    Spara namn

--- Hemläxor/v37/src/com/stuff/Main.java
+++ Hemläxor/v37/src/com/stuff/Main.java
@@ -6,6 +6,13 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+        // Definerar input som en ny Scanner.
+
+        System.out.print("Vad är ditt namn? ");
+        String namn = input.next();
+
+        System.out.println(namn);
+
+
     }
 }
