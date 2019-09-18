72d1147d6f1a942bc7cba585ad5cd43b14fe74d4
Vecopotryx <repsejk@gmail.com>
2019-09-18 17:47:28 +0200

    Finished project

--- Hemläxor/v38/src/com/stuff/Main.java
+++ Hemläxor/v38/src/com/stuff/Main.java
@@ -8,13 +8,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Skriv in 10 heltal: ");
-        int[] nummer = new int[10];
-        for (int i = 0; i < 10; i++) {
-            nummer[i] = input.nextInt();
+        int[] numbers = new int[10];
+        for (int i = 0; i < 10; i++) {     // put the input into array
+            numbers[i] = input.nextInt();
         }
-        System.out.println(Arrays.toString(nummer));
 
-        int min = Arrays.stream(nummer).min().getAsInt();
+        int max = Arrays.stream(numbers).max().getAsInt();
+        int min = Arrays.stream(numbers).min().getAsInt();
+
+        // calculate average
+        float sum = Arrays.stream(numbers).sum();
+        float average = sum / 10;
+
+        // results
+        System.out.println("--------------------------"); // for aesthetic reasons
+
+        System.out.println(Arrays.toString(numbers));
+        System.out.println("Högst: " + max);
         System.out.println("Lägst: " + min);
+        System.out.println("Medelvärde: " + average);
     }
 }
