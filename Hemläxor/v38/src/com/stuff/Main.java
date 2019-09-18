aaf7bcf804a95d7cdd13d2dcd0d988c1dbdf4e77
Vecopotryx <repsejk@gmail.com>
2019-09-18 17:32:03 +0200

    User input to array

---
+++ Hemläxor/v38/src/com/stuff/Main.java
@@ -0,0 +1,17 @@
+package com.stuff;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Skriv in 10 heltal: ");
+        int[] nummer = new int[10];
+        for (int i = 0; i < 10; i++) {
+            nummer[i] = input.nextInt();
+        }
+        System.out.println(Arrays.toString(nummer));
+    }
+}
