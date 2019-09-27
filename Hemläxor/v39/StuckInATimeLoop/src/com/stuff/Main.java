6b7ec9fb3ff9a3d75cb71a573c0a1040ecb9144d
Jesper <Repsejk@gmail.com>
2019-09-27 08:26:54 +0200

    Update Main.java

--- Hemläxor/v39/StuckInATimeLoop/src/com/stuff/Main.java
+++ Hemläxor/v39/StuckInATimeLoop/src/com/stuff/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
+        int N = input.nextInt() + 1;
         if (1 <= N && N <= 101) {
             for (int i = 1; i < N; i++) {
                 System.out.println(i + " Abracadabra");
