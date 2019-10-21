a0d3d42418fa531a02fd903b8404120677774c5c
Vecopotryx <repsejk@gmail.com>
2019-10-21 12:40:06 +0200

    Another commit

--- Hemläxor/v43/src/Main.java
+++ Hemläxor/v43/src/Main.java
@@ -5,6 +5,14 @@ public class Main {
         lol();
 
         min(2, 4);
+
+        count(4);
+
+        kelvin(20);
+
+        ageControl(99);
+
+        stair(4);
     }
 
     /**
@@ -49,6 +57,47 @@ public class Main {
         return kelvin;
     }
 
+    /**
+     * Don't judge me please, this is what the task tolddd me to do.
+     * @param age
+     */
     public static void ageControl(int age){
+        if(0 < age && age <= 5) {
+            System.out.println("Småbarn får inte göra något.");
+        }
+            else if(6 <= age && age <= 12) {
+              System.out.println("Du får spela Fortnite.");
+        }
+            else if(13 <= age && age <= 14){
+              System.out.println("Du är tonåring.");
+        }
+           else if(15 <= age && age <= 17){
+                System.out.println("Du får köra moppe.");
+        }
+            else if(18 <= age && age <= 20){
+                System.out.println("Du får köra bil.");
+        }
+            else if(21 <= age && age <= 64){
+                System.out.println("Du får vuxenstraff om du gör något dumt.");
+        }
+           else if(65 <= age && age <= 99){
+               System.out.println("Du är pensionär.");
+        }
+           else if(100 < age){
+              System.out.println("R.I.P?");
+        }
+    }
+
+    /**
+     *
+     * @param steps
+     */
+    public static void stair(int steps){
+        for(int i = 1; i <= steps; i++) {
+            for(int j = 0; j < i; j++) {
+                System.out.print("X");
+            }
+            System.out.println();
+        }
     }
 }
