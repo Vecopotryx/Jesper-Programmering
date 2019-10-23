e2870deb657f6a97cd327f734c54d1d486122a06
Vecopotryx <repsejk@gmail.com>
2019-10-23 11:53:06 +0200

    Started working on the assignement

--- Hemläxor/v43/src/Main.java
+++ Hemläxor/v43/src/Main.java
@@ -1,103 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-	// Detta är en lektionsuppgift, hamnade i hemläxor då jag inte tänkte särskilt mycket när jag skapade filen.
-        lol();
-
-        min(2, 4);
-
-        count(4);
-
-        kelvin(20);
-
-        ageControl(99);
-
-        stair(4);
+	// write your code here
+        System.out.println(volume(1));
     }
 
-    /**
-     * Task 1, prints "HAHAHAHAHAHAHA"
-     */
-    public static void lol() {
-        System.out.println("HAHAHAHAHAHAHA");
-    }
-
-    /**
-     * Prints the smallest value out of a and b.
-     * @param a - An integer
-     * @param b - An integer
-     * @return Returns the smallest value of a and b.
-     */
-    public static int min(int a, int b) {
-        if(a < b){
-            return a;
-        }
-        else {
-            return b;
-        }
-    }
-
-    /**
-     *
-     * @param n
-     */
-    public static void count(int n){
-        for(int i = 1; i <= n; i++){
-            System.out.println(i);
-        }
-    }
-
-    /**
-     *
-     * @param celsius
-     * @return
-     */
-    public static double kelvin(double celsius){
-        double kelvin = celsius + 273.15;
-        return kelvin;
-    }
-
-    /**
-     * Don't judge me please, this is what the task tolddd me to do.
-     * @param age
-     */
-    public static void ageControl(int age){
-        if(0 < age && age <= 5) {
-            System.out.println("Småbarn får inte göra något.");
-        }
-            else if(6 <= age && age <= 12) {
-              System.out.println("Du får spela Fortnite.");
-        }
-            else if(13 <= age && age <= 14){
-              System.out.println("Du är tonåring.");
-        }
-           else if(15 <= age && age <= 17){
-                System.out.println("Du får köra moppe.");
-        }
-            else if(18 <= age && age <= 20){
-                System.out.println("Du får köra bil.");
-        }
-            else if(21 <= age && age <= 64){
-                System.out.println("Du får vuxenstraff om du gör något dumt.");
-        }
-           else if(65 <= age && age <= 99){
-               System.out.println("Du är pensionär.");
-        }
-           else if(100 < age){
-              System.out.println("R.I.P?");
-        }
-    }
-
-    /**
-     *
-     * @param steps
-     */
-    public static void stair(int steps){
-        for(int i = 1; i <= steps; i++) {
-            for(int j = 0; j < i; j++) {
-                System.out.print("X");
-            }
-            System.out.println();
-        }
+    public static double volume(double radius){
+        double volume = Math.PI * Math.pow(radius, 3) * 4/3;
+        return volume;
     }
 }
