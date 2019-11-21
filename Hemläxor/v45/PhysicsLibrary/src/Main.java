bdfdd13335515a05dd98af764720f5f310943eda
Vecopotryx <Repsejk@gmail.com>
2019-11-21 10:31:37 +0100

    Added a few more constants

--- Hemläxor/v45/PhysicsLibrary/src/Main.java
+++ Hemläxor/v45/PhysicsLibrary/src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
 
-    static double g_swe = 9.82; // Acceleration of gravity (Sweden)
-    static double G = 6.674E-11; // Gravitational constant
+    static double g_swe = 9.82; // Acceleration of gravity in Sweden (m/s^2)
+    static double G = 6.674E-11; // Gravitational constant (Nm^2 / kg^2)
+    static double R = 8.3145; // Gas constant (J)
+    static double p_0 = 1013.25; // Atmospheric pressure at sea-level (hPa)
+    static double c = 2.99792458E8; // Speed of light in a vacuum (m/s)
 
     public static void main(String[] args) {
         // Hi there :)
