2e48befb39795b8e3e02a2932aa548cc4fc09455
Jesper <Repsejk@gmail.com>
2020-04-24 09:35:51 +0200

    Hangman - More work on user input in game screen

--- Hangman/src/GameControl.java
+++ Hangman/src/GameControl.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+
 public class GameControl {
     public static void startGame(){
         HandleFile.randomFromFile("wordlist.txt");
         System.out.println(Model.get_answer());
         System.out.println(getAnswerLength());
-        gameScreen();
+        while(true){
+            gameScreen();
+        }
         // char[] guesses = {'b', 'j', 'c'};
         // Model.set_incorrectCharGuesses(guesses);
         // Interface.displayWord();
@@ -17,9 +21,16 @@ public class GameControl {
         return Model.get_answer().length();
     }
 
+    public static ArrayList<Character> charGuess = new ArrayList<Character>();;
     public static void gameScreen(){
         Interface.printGameScreen();
         String stringIn = Input.getStringInput();
         System.out.println(stringIn);
+        if(stringIn.length() > 1){
+
+        } else {
+            charGuess.add(stringIn.toCharArray()[0]);
+        }
+        System.out.println(charGuess);
     }
 }
